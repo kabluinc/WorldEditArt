b7def34348a5dde1d04d2480a5f55e5eadd06860
PEMapModder <pemapmodder1970@gmail.com>
2016-02-26 00:09:13 +0800

    WorldEditArt Delta: rewrite, initial commit

--- guide/doctree.js
+++ guide/doctree.js
@@ -1,14 +1,14 @@
 /*
  * WorldEditArt
  *
- * Copyright (C) 2015 PEMapModder
+ * Copyright (C) 2015-2016 LegendsOfMCPE
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PEMapModder
+ * @author LegendsOfMCPE
  */
 
 var runtimeAutoId = 0;

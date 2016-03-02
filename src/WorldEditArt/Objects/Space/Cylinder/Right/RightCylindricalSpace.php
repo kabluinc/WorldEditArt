dcfa1390de89d8d36581ec52e05528568e9d895c
PEMapModder <pemapmodder1970@gmail.com>
2016-03-02 23:11:44 +0800

    Added sphere

---
+++ src/WorldEditArt/Objects/Space/Cylinder/Right/RightCylindricalSpace.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * WorldEditArt
+ *
+ * Copyright (C) 2016 PEMapModder
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PEMapModder
+ */
+
+namespace WorldEditArt\Objects\Space\Cylinder\Right;
+
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+
+class RightCylindricalSpace{
+	public function __construct(Level $level, Vector3 $baseCenter){
+	}
+}
